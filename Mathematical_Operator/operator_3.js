3fc1877a5b04ce80584c136e228b350e8e7d956b
ankita-barad <121330309+ankita-barad@users.noreply.github.com>
2022-12-28 13:44:39 +0530

    Update operator_3.js

--- Mathematical_Operator/operator_3.js
+++ Mathematical_Operator/operator_3.js
@@ -2,5 +2,5 @@
 
 function productOfSix(one, two, three, four, five, six) {
     // Write code here
-    console.log(one*two*three*four*five*six*seven)
+    console.log(one*two*three*four*five*six)
 }
